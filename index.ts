5b016f12dca94eabc217dcf25cf00debe11e0a42
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2026-02-15 21:15:22 +0200

    added shadcn

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrent from "concurrently";
+
+concurrent([
+    { 
+        name: "server",
+        command: "bun run dev",
+        cwd: "packages/server",
+        prefixColor: "red",
+    },
+    { 
+        name: "client",
+        command: "bun run dev",
+        cwd: "packages/client",
+        prefixColor: "green",
+    },
+])
