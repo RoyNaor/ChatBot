370a12780c73b24132f933d88b875e4eff4445d5
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2026-02-15 21:41:07 +0200

    set prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,20 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     fetch('/api/hello')
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
-      .catch((err) => console.error(err))
-  }, [])
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div>
       <h1>{message}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
